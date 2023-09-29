9771682504d32e856cd018a7f080b652857c31cf
Lukasz Mazur <lukasz.mazur.wroclaw@gmail.com>
2023-09-29 01:22:55 +0100

    commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route as Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Public/Home', [
+        'laravel' => \Illuminate\Foundation\Application::VERSION,
+        'php' => PHP_VERSION,
+        'test' => 'Tester'
+    ]);
+});
